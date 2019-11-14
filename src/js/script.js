6c2de3bc8fa34c955f24b00a4c208cd49842e02f
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-14 09:43:23 +0100

    Add css, HTML, JS function and script mod of new chapter

--- src/js/script.js
+++ src/js/script.js
@@ -6,6 +6,7 @@
   const select = {
     templateOf: {
       menuProduct: '#template-menu-product',
+      cartProduct: '#template-cart-product', // CODE ADDED
     },
     containerOf: {
       menu: '#product-list',
@@ -26,11 +27,31 @@
     },
     widgets: {
       amount: {
-        input: 'input[name="amount"]',
+        input: 'input.amount', // CODE CHANGED
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
     },
+    // CODE ADDED START
+    cart: {
+      productList: '.cart__order-summary',
+      toggleTrigger: '.cart__summary',
+      totalNumber: `.cart__total-number`,
+      totalPrice: '.cart__total-price strong, .cart__order-total .cart__order-price-sum strong',
+      subtotalPrice: '.cart__order-subtotal .cart__order-price-sum strong',
+      deliveryFee: '.cart__order-delivery .cart__order-price-sum strong',
+      form: '.cart__order',
+      formSubmit: '.cart__order [type="submit"]',
+      phone: '[name="phone"]',
+      address: '[name="address"]',
+    },
+    cartProduct: {
+      amountWidget: '.widget-amount',
+      price: '.cart__product-price',
+      edit: '[href="#edit"]',
+      remove: '[href="#remove"]',
+    },
+    // CODE ADDED END
   };
 
   const classNames = {
@@ -38,6 +59,11 @@
       wrapperActive: 'active',
       imageVisible: 'active',
     },
+    // CODE ADDED START
+    cart: {
+      wrapperActive: 'active',
+    },
+    // CODE ADDED END
   };
 
   const settings = {
@@ -45,13 +71,20 @@
       defaultValue: 1,
       defaultMin: 1,
       defaultMax: 9,
-    }
+    }, // CODE CHANGED
+    // CODE ADDED START
+    cart: {
+      defaultDeliveryFee: 20,
+    },
+    // CODE ADDED END
   };
 
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    // CODE ADDED START
+    cartProduct: Handlebars.compile(document.querySelector(select.templateOf.cartProduct).innerHTML),
+    // CODE ADDED END
   };
-
   class Product {
     constructor(id, data) {
       const thisProduct = this;
@@ -66,7 +99,6 @@
       thisProduct.initAmountWidget();
       thisProduct.processOrder();
 
-      console.log('new Product -- thisProduct:', thisProduct);
 
     }
     renderInMenu() {
@@ -98,7 +130,7 @@
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
       thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget);
-      console.log('thisProduct.amountWidgetElem', thisProduct.amountWidgetElem);
+
 
     }
 
@@ -170,7 +202,6 @@
 
       /* set variable price to equal thisProduct.data.price */
       let price = thisProduct.data.price;
-      console.log(price);
 
       /* START LOOP: for each paramId in thisProduct.data.params */
       for (let paramId in thisProduct.data.params) {
@@ -187,7 +218,6 @@
 
           /* START IF: if option is selected and option is not default */
           if(optionSelected && !option.default){
-            //console.log('optionSelected',optionSelected);
 
             /* add price of option to variable price */
             price += option.price ;
@@ -227,8 +257,7 @@
       price *= thisProduct.amountWidget.value;
 
       thisProduct.priceElem.innerHTML = price;
-      console.log(price);
-    }
+      }
 
     initAmountWidget() {
       const thisProduct = this;
@@ -251,8 +280,6 @@
       thisWidget.value = settings.amountWidget.defaultValue;
       thisWidget.setValue(thisWidget.input.value);
       thisWidget.initActions(thisWidget.value);
-      console.log('AmountWidget -- thisWidget :', thisWidget );
-      console.log('constructor arguments -- element', element);
     }
 
     getElements(element) {
@@ -295,20 +322,13 @@
       thisWidget.linkIncrease.addEventListener('click', function(event) {
 
         event.preventDefault();
-        console.log(thisWidget.value);
         thisWidget.setValue(thisWidget.value + 1);
-        // console.log(thisWidget.value);
-        // thisWidget.input.value = thisWidget.value;
-        // thisWidget.value = price;
       });
 
       thisWidget.linkDecrease.addEventListener('click', function(event) {
 
         event.preventDefault();
-        console.log(thisWidget.value);
         thisWidget.setValue(thisWidget.value - 1);
-        // console.log(thisWidget.value);
-        // thisWidget.input.value = thisWidget.value;
       });
 
     }
