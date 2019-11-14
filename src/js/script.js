99036cf409a7870177f2f131dec3a13eb2ebbb80
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-14 13:03:38 +0100

    Add method initCart & call them in const app

--- src/js/script.js
+++ src/js/script.js
@@ -356,6 +356,7 @@
   class Cart {
     constructor(element) {
       const thisCart = this;
+      console.log(thisCart);
 
       thisCart.products = [];
 
@@ -388,6 +389,13 @@
       thisApp.data = dataSource;
     },
 
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function() {
       const thisApp = this;
       console.log('*** App starting ***');
@@ -397,6 +405,7 @@
       console.log('templates:', templates);
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
 
     },
   };
