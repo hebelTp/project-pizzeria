67b0d53ce60a43eae41aeccc4342cf6611c465d6
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-14 10:35:24 +0100

    Add new Class

--- src/js/script.js
+++ src/js/script.js
@@ -342,6 +342,25 @@
 
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+      thisCart.getElements(element);
+      console.log('new Cart --> this.cart', thisCart);
+    }
+
+    getElements(element) {
+
+      const thisCart = this;
+
+      thisCart.dom = {};
+      thisCart.dom.wrapper = element;
+    }
+
+  }
+
   const app = {
     initMenu: function() {
       const thisApp = this;
