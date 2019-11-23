1ac945f944486ec17805932c064867265fc3186a
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-23 14:12:06 +0100

    Fix import path

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,6 @@
 /* global Handlebars, dataSource */
 
-const utils = {}; // eslint-disable-line no-unused-vars
+export const utils = {}; // eslint-disable-line no-unused-vars
 
 utils.createDOMFromHTML = function(htmlString) {
   let div = document.createElement('div');
@@ -53,4 +53,4 @@ Handlebars.registerHelper('joinValues', function(input, options) {
   return Object.values(input).join(options.fn(this));
 });
 
-export default utils;
+
