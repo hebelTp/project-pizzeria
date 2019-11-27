c136748380766842026dd96052679c12d7ec2a3c
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-27 13:40:46 +0100

    Fix problem with wrong css&js files  range slider

---
+++ src/js/components/HourPicker.js
@@ -0,0 +1,20 @@
+import BaseWidget from './BaseWidget.js';
+import {utils} from '../utils.js';
+import {settings, select} from '../settings.js';
+
+class HourPicker extends BaseWidget {
+  constructor(wrapper){
+    super(wrapper, settings.hours.open);
+    const thisWidget = this;
+  }
+
+
+
+
+
+
+}
+
+export default HourPicker;
+
+
