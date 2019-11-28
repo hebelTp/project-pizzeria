336a3ac14378c4da7b31289d67882780128c99a4
Tomek Piotrowski <tomasz.piotrowski.tp@gmail.com>
2019-11-28 13:33:30 +0100

    Add getData() into Booking complete adresses

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -17,15 +17,23 @@ class Booking {
   getData(){
     const thisBooking = this;
 
+    const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
+    const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
+
     const params = {
       booking: [
+        startDateParam,
+        endDateParam,
 
       ],
       eventsCurrent: [
-
+        settings.db.notRepeatParam,
+        startDateParam,
+        endDateParam,
       ],
       eventsRepeat: [
-
+        settings.db.repeatParam,
+        endDateParam,
       ],
 
     };
@@ -33,11 +41,14 @@ class Booking {
     console.log('getData params', params);
 
     const urls ={
-      booking:        settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
-      eventsCurrent:  settings.db.url + '/' + settings.db.event   + '?' + params.booking.join('&'),
-      eventsRepeat:   settings.db.url + '/' + settings.db.event   + '?' + params.booking.join('&'),
+      booking:        settings.db.url + '/' + settings.db.booking
+                                      + '?' + params.booking.join('&'),
+      eventsCurrent:  settings.db.url + '/' + settings.db.event
+                                      + '?' + params.eventsCurrent.join('&'),
+      eventsRepeat:   settings.db.url + '/' + settings.db.event
+                                      + '?' + params.eventsRepeat.join('&'),
     };
-
+    console.log('getData urls', urls);
 
   }
 
